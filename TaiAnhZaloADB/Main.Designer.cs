d1eccdf247490c6cb8a40af89ef85ca42d5b4a46
DuongDev98 <duongdev98@gmail.com>
2022-09-12 23:41:31 +0700

    init

---
+++ TaiAnhZaloADB/Main.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace TaiAnhZaloADB
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnTaiAnh = new System.Windows.Forms.Button();
+            this.btnDung = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.lblThongBao = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTaiAnh
+            // 
+            this.btnTaiAnh.Location = new System.Drawing.Point(12, 12);
+            this.btnTaiAnh.Name = "btnTaiAnh";
+            this.btnTaiAnh.Size = new System.Drawing.Size(90, 48);
+            this.btnTaiAnh.TabIndex = 0;
+            this.btnTaiAnh.Text = "Tải ảnh";
+            this.btnTaiAnh.UseVisualStyleBackColor = true;
+            // 
+            // btnDung
+            // 
+            this.btnDung.Location = new System.Drawing.Point(107, 12);
+            this.btnDung.Name = "btnDung";
+            this.btnDung.Size = new System.Drawing.Size(90, 48);
+            this.btnDung.TabIndex = 1;
+            this.btnDung.Text = "Dừng";
+            this.btnDung.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 67);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(547, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // lblThongBao
+            // 
+            this.lblThongBao.AutoSize = true;
+            this.lblThongBao.Location = new System.Drawing.Point(203, 30);
+            this.lblThongBao.Name = "lblThongBao";
+            this.lblThongBao.Size = new System.Drawing.Size(74, 13);
+            this.lblThongBao.TabIndex = 3;
+            this.lblThongBao.Text = "THÔNG BÁO:";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 125);
+            this.Controls.Add(this.lblThongBao);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnDung);
+            this.Controls.Add(this.btnTaiAnh);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tải ảnh zalo";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTaiAnh;
+        private System.Windows.Forms.Button btnDung;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.Label lblThongBao;
+    }
+}
---
+++ TaiAnhZaloADB/Main.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KAutoHelper;
+
+namespace TaiAnhZaloADB
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            FormClosing += Main_FormClosing;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            loadding(false);
+            System.Threading.Thread.Sleep(200);
+        }
+
+        Bitmap imgShowMore, imgDownload;
+        BackgroundWorker backgroundWorker;
+        private void Main_Load(object sender, EventArgs e)
+        {
+            lblThongBao.Text = "";
+
+            backgroundWorker = new BackgroundWorker();
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+
+            progressBar.Visible = false;
+            progressBar.Step = 1;
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+
+            tmr.Tick += Tmr_Tick;
+            btnTaiAnh.Click += BtnTaiAnh_Click;
+            btnDung.Click += BtnDung_Click;
+
+            //tải ảnh
+            imgShowMore = (Bitmap)Bitmap.FromFile("showmore.png");
+            imgDownload = (Bitmap)Bitmap.FromFile("textdownload.png");
+        }
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            loadding(false);
+        }
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //Kiểm tra xem có thiết bị nào đang kết nối hay không
+            var lstDevices = ADBHelper.GetDevices();
+            if (lstDevices.Count == 0)
+            {
+                ShowWaring("Không có thiết bị nào được kết nối");
+                return;
+            }
+
+            if (lstDevices.Count > 1)
+            {
+                ShowWaring("Chỉ có thể kết nối 1 thiết bị 1 lúc");
+                return;
+            }
+
+            string deviceId = lstDevices[0];
+
+            bool next;
+            while (!stop)
+            {
+                try
+                {
+                    next = false;
+                    Point? point = null;
+                    //b1: tìm dấu 3 chấm, ko có thì chạm vào màn hình
+                    while (!stop && !next)
+                    {
+                        DatThongBaoThread("Tìm dấu 3 chấm");
+                        var screen = ADBHelper.ScreenShoot(deviceId);
+                        point = ImageScanOpenCV.FindOutPoint(screen, imgShowMore);
+                        if (point != null)
+                        {
+                            next = true;
+                        }
+                        else
+                        {
+                            //nhấn màn 1 cái rồi tìm tiếp
+                            System.Threading.Thread.Sleep(2000);
+                            ADBHelper.TapByPercent(deviceId, 50, 50);
+                        }
+                    }
+                    //b2: nhấn 3 chấm
+                    next = false;
+                    while (!stop && !next)
+                    {
+                        DatThongBaoThread("Nhấn dấu 3 chấm");
+                        point = point ?? new Point(0,0);
+                        ADBHelper.Tap(deviceId, point.Value.X, point.Value.Y);
+                        next = true;
+                    }
+                    //b3: nhấn tải ảnh
+                    next = false;
+                    while (!stop && !next)
+                    {
+                        DatThongBaoThread("Nhấn tải ảnh");
+                        var screen = ADBHelper.ScreenShoot(deviceId);
+                        point = ImageScanOpenCV.FindOutPoint(screen, imgDownload);
+                        if (point != null)
+                        {
+                            ADBHelper.Tap(deviceId, point.Value.X, point.Value.Y);
+                            next = true;
+                        }
+                    }
+                    //b4: lướt sang ảnh khác
+                    next = false;
+                    while (!stop && !next)
+                    {
+                        DatThongBaoThread("Lướt cái tiếp theo");
+                        ADBHelper.Swipe(deviceId, 200, 300, 10, 300, 100);
+                        next = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    DatThongBaoThread("Error: " + ex.Message);
+                    stop = true;
+                }
+            }
+        }
+
+        private void DatThongBaoThread(string text)
+        {
+            Invoke(new MethodInvoker(delegate {
+                lblThongBao.Text = text;
+            }));
+        }
+
+        private void Tmr_Tick(object sender, EventArgs e)
+        {
+            tmr.Enabled = false;
+            if (progressBar.Value == progressBar.Maximum)
+            {
+                progressBar.Value = progressBar.Minimum;
+            }
+            else
+            {
+                progressBar.PerformStep();
+            }
+            tmr.Enabled = true;
+        }
+        private void BtnDung_Click(object sender, EventArgs e)
+        {
+            loadding(false);
+        }
+        private void loadding(bool run)
+        {
+            stop = !run;
+            tmr.Enabled = run;
+            progressBar.Visible = run;
+            if (!run) progressBar.Value = 0;
+            btnTaiAnh.Enabled = !run;
+            if (!run && !lblThongBao.Text.Contains("Error:")) lblThongBao.Text = "";
+        }
+
+        private bool stop = true;
+        private void BtnTaiAnh_Click(object sender, EventArgs e)
+        {
+            loadding(true);
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void ShowWaring(string text)
+        {
+            MessageBox.Show(text, "Cảnh báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+    }
+}
